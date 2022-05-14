157cf577e0080090b211434bb14f7cd68f808c26
Hakim0611 <hakim0611066@gmail.com>
2022-05-14 20:36:54 +0500

    14 20.36

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,12 @@
-const humburger = document.querySelector('.humburger'),
-		menu = document.querySelector('.menu'),
-		closeElem = document.querySelector('.menu__close');
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
 
-humburger.addEventListener('click', () => {
-	menu.classList.add('active');
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
-	menu.classList.remove('active');
+    menu.classList.remove('active');
 });
+
